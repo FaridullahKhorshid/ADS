eda99761801e326e2676c2e8cc1e5c0978c26ea5
Bram Delver <bramdelver@gmail.com>
2021-10-06 19:40:47 +0200

    Purchase toString

--- Assignment2/src/main/java/models/Purchase.java
+++ Assignment2/src/main/java/models/Purchase.java
@@ -1,7 +1,9 @@
 package models;
 
+import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 public class Purchase {
     private final Product product;
@@ -75,4 +77,26 @@ public class Purchase {
     }
 
     // TODO add public and private methods as per your requirements
+
+    public double getTotal() {
+        return this.getCount() * this.getProduct().getPrice();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.getBarcode());
+        sb.append("/");
+        sb.append(this.getProduct().getTitle());
+        sb.append("/");
+        sb.append(this.getCount());
+        sb.append("/");
+
+        // Set the locale to use a period instead of a comma for fractionals
+        NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.UK);
+        // Print trailing zeroes up to and including the second fractional
+        numberFormat.setMinimumFractionDigits(2);
+        sb.append(numberFormat.format(this.getTotal()));
+        return sb.toString();
+    }
 }
